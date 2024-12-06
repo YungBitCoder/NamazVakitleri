9e36c57c59f1f1414a7978eadbfcded090ecb142
YungBitCoder <altandag5@gmail.com>
2024-12-06 18:48:46 +0300

    aaa
    asa

---
+++ script.js
@@ -0,0 +1,16 @@
+fetch('data.json')
+.then(response => {
+    if(!response.ok) {
+        throw new Error('Failed to load data.json');
+    }
+    return response.json;
+})
+.then(data => {
+    console.log('Fetched data:', data);
+    document.getElementById('brawl_stars').textContent = `Imsak: ${data[0].Imsak}`;
+    //document.getElementById('ogle').textContent = `Ogle: ${data[0].Ogle}`;
+    //document.getElementById('yatsi').textContent = `Yatsi: ${data[0].Yatsi}`;
+})
+.catch(error => {
+    console.error("Failed to fetch data", error);
+});
